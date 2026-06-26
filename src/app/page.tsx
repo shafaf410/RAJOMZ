84c6ca1d32573a82719285e91a864965fb499fec
shafaf <shafaf410@gmail.com>
2026-06-26 06:17:43 +0530

    style: remove trust section stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import HeroSection from "@/components/sections/HeroSection";
-import TrustSection from "@/components/sections/TrustSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import IndustriesSection from "@/components/sections/IndustriesSection";
 import WhyChooseUsSection from "@/components/sections/WhyChooseUsSection";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
-      <TrustSection />
       <ServicesSection />
       <IndustriesSection />
       <WhyChooseUsSection />
